05b8783f52f5a8b6784743731292ffd6b3aff148
Philip Cutler <cutlervufamily@gmail.com>
2023-02-04 21:31:55 +1030

    added garden bed classes and api

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
-import json
-import pathlib
-from typing import List
+# import external modules
 
-from fastapi import Depends, FastAPI, HTTPException, Query
+from fastapi import Depends, FastAPI, HTTPException, Header, Query, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
 from sqlmodel import Session, select
 
+from typing import List, Optional
+
+# import local modules
+
 from database import engine, create_db_and_tables
-from models import Planting, PlantingCreate, PlantingRead, PlantingUpdate
+from models import Bed, BedCreate, BedRead, BedUpdate, Planting, PlantingCreate, PlantingRead, PlantingUpdate, Bed
 
 # instantiate the FastAPI app
 app = FastAPI()
 
-# create container for our data - to be loaded at app startup.
-data = []
+templates = Jinja2Templates(directory="templates")
 
 
 @app.on_event("startup")
 def on_startup():
   create_db_and_tables()
+
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request,
+          hx_request: Optional[str] = Header(None),
+          ):
+  plantings = [
+    {'plant': 'cucumber', 'variety': 'lebanese', 'notes': ''}
+  ]
+  context = {"request": request, "plantings": plantings}
+  if hx_request:
+    return templates.TemplateResponse("table.html", context)
+  return templates.TemplateResponse("index.html", context)
 
 
 # See https://sqlmodel.tiangolo.com/tutorial/fastapi/session-with-dependency/
@@ -68,7 +85,7 @@ def update_planting(*,
                     ):
     db_planting = session.get(Planting, planting_id)
     if not db_planting:
-        raise HTTPException(status_code=404, detail="planting not found")
+        raise HTTPException(status_code=404, detail="Planting not found")
     # update the planting data
     planting_data = planting.dict(exclude_unset=True)
     for key, val in planting_data.items():
@@ -84,10 +101,76 @@ def delete_planting(*,
                     session: Session = Depends(get_session),
                     planting_id: int,
                     ):
-    db_plantings = session.get(Planting, planting_id)
-    if not db_plantings:
-        raise HTTPException(status_code=404, detail="planting not found")
-    session.delete(db_plantings)
+    db_planting = session.get(Planting, planting_id)
+    if not db_planting:
+        raise HTTPException(status_code=404, detail="Planting not found")
+    session.delete(db_planting)
+    session.commit()
+    return {"ok": True}
+
+
+@app.post("/beds/", response_model=BedRead)
+def create_bed(*,
+               session: Session = Depends(get_session),
+               bed: BedCreate
+               ):
+    db_bed = Bed.from_orm(bed)
+    session.add(db_bed)
+    session.commit()
+    session.refresh(db_bed)
+    return db_bed
+
+
+@app.get("/beds/", response_model=List[BedRead])
+def read_beds(*,
+              session: Session = Depends(get_session),
+              offset: int = 0,
+              limit: int = Query(default=100, lte=100)
+              ):
+    # select * from
+    stmt = select(Bed).offset(offset).limit(limit)
+    db_beds = session.exec(stmt).all()
+    return db_beds
+
+
+@app.get("/beds/{bed_id}", response_model=BedRead)
+def read_bed(*, session: Session = Depends(get_session), bed_id: int):
+    # find the planting with the given ID, or None if it does not exist
+    db_bed = session.get(Bed, bed_id)
+    if not db_bed:
+        raise HTTPException(status_code=404, detail="Bed not found")
+    return db_bed
+
+
+
+@app.patch("/beds/{bed_id}", response_model=BedRead)
+def update_bed(*,
+               session: Session = Depends(get_session),
+               bed_id: int,
+               bed: BedUpdate,
+               ):
+    db_bed = session.get(Bed, bed_id)
+    if not db_bed:
+        raise HTTPException(status_code=404, detail="Bed not found")
+    # update the planting data
+    bed_data = bed.dict(exclude_unset=True)
+    for key, val in bed_data.items():
+        setattr(db_bed, key, val)
+    session.add(db_bed)
+    session.commit()
+    session.refresh(db_bed)
+    return db_bed
+
+
+@app.delete("/beds/{bed_id}")
+def delete_bed(*,
+               session: Session = Depends(get_session),
+               bed_id: int,
+               ):
+    db_bed = session.get(Bed, bed_id)
+    if not db_bed:
+        raise HTTPException(status_code=404, detail="Bed not found")
+    session.delete(db_bed)
     session.commit()
     return {"ok": True}
 
--- models.py
+++ models.py
@@ -1,19 +1,45 @@
 from datetime import datetime
 from typing import List, Optional
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Relationship, SQLModel
+
+
+class BedBase(SQLModel):
+  name: str = Field(index=True)
+
+
+class Bed(BedBase, table=True):
+  id: Optional[int] = Field(default=None, primary_key=True)
+  plantings: List["Planting"] = Relationship(back_populates="bed")
+
+
+class BedCreate(BedBase):
+  pass
+
+
+class BedRead(BedBase):
+  id: int
+  
+  
+class BedUpdate(SQLModel):
+  name: Optional[str] = None
+  
 
 class PlantingBase(SQLModel):
-  plant: str = Field(index=True)
+  name: str = Field(index=True)
+  plant: str
   variety: Optional[str]
   # date_planted: Optional[datetime]
   # date_first_harvested: Optional[datetime]
   # date_removed: Optional[datetime]
   notes: Optional[str]
+  bed_id: Optional[int] = Field(default=None, foreign_key="bed.id")
+  
   
 class Planting(PlantingBase, table=True):
   id: Optional[int] = Field(default=None, primary_key=True)
   # date_planted: datetime
+  bed: Optional[Bed] = Relationship(back_populates="plantings")
 
 class PlantingCreate(PlantingBase):
   pass
@@ -28,3 +54,4 @@ class PlantingUpdate(SQLModel):
   # date_first_harvested: Optional[datetime]
   # date_removed: Optional[datetime]
   notes: Optional[str]
+
